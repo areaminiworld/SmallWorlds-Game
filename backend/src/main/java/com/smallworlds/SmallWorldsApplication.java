08767aa6bca513bb2c07909b6fc9d10c234d3bd3
areaminiworld <areaminimania@gmail.com>
2026-03-29 09:49:10 -0300

    Create main Spring Boot application class SmallWorldsApplication.java

---
+++ backend/src/main/java/com/smallworlds/SmallWorldsApplication.java
@@ -0,0 +1,11 @@
+package com.smallworlds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallWorldsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmallWorldsApplication.class, args);
+    }
+}
